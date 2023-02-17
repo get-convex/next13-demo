472d7a0b16670ca8e235d3babc153379eb864c4a
Preslav Le <presley@convex.dev>
2023-02-16 22:31:00 -0800

    Update layout.tsx

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -67,7 +67,7 @@ export default async function Layout(props: { children: React.ReactNode }) {
           This prevents hydration mismatches but causes hydration to suspend,
           delaying TTI. <b>Warning</b>: We have observed the following{" "}
           <a href="https://github.com/facebook/react/issues/25964">React bug</a>{" "}
-          if injectToStream is not available and more than one{" "}
+          when <code>nextInjectIntoStream()</code> is not available and more than one{" "}
           <code>useQuery()</code> hook suspend during hydration.
         </p>
       </article>
